b19ed06a4ae649e38ae4f8dfb9a1e899ccfd93d6
Sugarkhuu <sugarkhuul@gmail.com>
2023-09-22 15:15:47 +0200

    second

---
+++ housedata.sql
@@ -0,0 +1,56 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.5
+-- Dumped by pg_dump version 14.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: housenew; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.housenew (
+    text_ad text,
+    price text
+);
+
+
+ALTER TABLE public.housenew OWNER TO postgres;
+
+--
+-- Data for Name: housenew; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.housenew (text_ad, price) FROM stdin;
+Хотын төв 5 р сургуулийн хойно 2 өрөө байр	140 сая ₮
+Бзд офицерын тойргийн зүүн талд 43.60 мкв 2 өрөө байр	95 сая ₮
+Бгд, нарны гүүрний баруун талд алтай хотхонд 48мк2 2 өрөө	125 сая ₮
+Төмөр замд гал тогоо тусдаа баруун харсан 2өрөө байр 31м2	135 сая ₮
+Зайсан astra villa хотхонд 2 өрөө	230 сая ₮
+Нарантуул зах ард нарт хотхон 2 өрөө 50.75m2	132 сая ₮
+River garden ы ард modun town 2 өрөө	257 сая ₮
+Глобал гарден хотхонд гал тогоо тусдаа 62.5мкв 2 өрөө	187.5 сая ₮
+Tsaiz zahiin uulzwar sky residence 62мкв 2 өрөө	160 сая ₮
+Худ, маршалын гүүрний баруун талд king tower-д 71мкв 2 өрөө байр	3.7 сая ₮
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
